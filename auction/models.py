d2b22dda7c99025726b7ff6cee198a07f8ad6274
Sadullo Obidjonov <sizning_emailingiz@gmail.com>
2026-05-04 15:22:22 +0500

    Auction project: 1-bosqich yakunlandi

---
+++ auction/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+
+class User(AbstractUser):
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)#nmaga bu yerda default ga 0.00 berilgan
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+
+        return self.username
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+class Product(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='products')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    start_price = models.DecimalField(max_digits=10, decimal_places=2)
+    current_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Bid(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='bids')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.amount} ({self.product.title})"
